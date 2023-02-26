bd6d9618305a3482ca5162c5f5794c82ed43bb77
luluxing <luluxing1988@gmail.com>
2023-02-25 21:30:40 -0500

    Fix the python version issue

--- lib/python/XMLUtils.py
+++ lib/python/XMLUtils.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # This file is part of VoltDB.
 # Copyright (C) 2008-2020 VoltDB Inc.
 #
--- tools/getgitinfo.py
+++ tools/getgitinfo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2.7
 from subprocess import Popen, PIPE
 import sys
 import re
